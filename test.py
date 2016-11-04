12f9b48dff807fb515d2183618880557805e7519
Chris Wu <chris.wu@goodix.com>
2016-11-04 17:20:40 +0800

    move printinfo into test2.py

--- test.py
+++ test.py
@@ -2,19 +2,22 @@
 # -*- coding: UTF-8 -*-
 
 
-def printinfo( arg1, *vartuple ):
-   "打印任何传入的参数"
-   print "输出: "
-   print arg1
-   for var in vartuple:
-      arg1 = arg1 + var
-   print arg1
+import test2
+
+def sum(arg1, arg2):
+	arg3 = arg1+arg2
+	return arg3
+
+print sum(5,6)
 
 
-print printinfo(1,2,3,4,5)
+print test2.printinfo(1,2,3,4,5)
 
 
 sum = lambda arg1, arg2: arg1 + arg2;
 
 print sum(2,2)
+
+
+
 
