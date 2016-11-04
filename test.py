0f2ee71bcd704c7c316ef7c3dfdf4c00f427b92e
Chris Wu <chris.wu@goodix.com>
2016-11-04 15:41:36 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+def printinfo( arg1, *vartuple ):
+   "打印任何传入的参数"
+   print "输出: "
+   print arg1
+   for var in vartuple:
+      arg1 = arg1 + var
+   print arg1
+
+
+print printinfo(1,2,3,4,5)
+
